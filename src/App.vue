5faa7de058b2d6d730d5470a4ebb99ce1c9b7eff
Sebastian Bonato <sebabonato12@gmail.com>
2019-03-30 21:42:51 -0300

    Agregue Comentarios en las Funciones

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-      <form v-if="this.show" @submit.prevent="postPeliculas()">
-          <input type="text" v-model="pelicula.titulo">
-          <input type="text" v-model="pelicula.año">
-          <button type="submit">Guardar</button>
-      </form>
+      <div>
+        <form v-if="this.show" @submit.prevent="postPeliculas()">
+            <input type="text" v-model="pelicula.titulo">
+            <input type="text" v-model="pelicula.año">
+            <button type="submit">Guardar</button>
+        </form>
+      </div>
       <div>
         <button v-on:click="getPeliculas()">Peliculas</button>
       </div>
@@ -37,7 +39,6 @@ export default {
        pelicula : new Pelicula(),
        show: true,
        peliculas: [],
-       control: ''
     }
   },
   created(){
@@ -52,12 +53,14 @@ export default {
   },
   methods:{
     //Funcion Exclusiva de Vue para crear los Metodos o Funciones
+    //Funcion de Axios para traer Informacion desde el Servidor
     getPeliculas(){
       axios.get('https://api.themoviedb.org/3/discover/movie?api_key=1b62ccff88d2cd537027e1d82920197b&primary_release_date.gte=2018-05-15&primary_release_date.lte=2018-7-28').then((response)=>{
           console.log(response.data.results);
           this.peliculas = response.data.results;
       }).then(alert("Carga Correcta"));
     },
+    //Funcion de Axios para enviar informacion al Servidor
     postPeliculas(){
       if (this.pelicula.titulo && this.pelicula.año){
         axios.post('https://api.themoviedb.org/3/discover/movie?api_key=1b62ccff88d2cd537027e1d82920197b&primary_release_date.gte=2018-05-15&primary_release_date.lte=2018-7-28',
